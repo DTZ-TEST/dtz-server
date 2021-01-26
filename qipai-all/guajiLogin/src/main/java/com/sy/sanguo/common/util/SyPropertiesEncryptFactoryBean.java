f8a00f649e34cd23d1b6c6741c63ec0de6074a7f
Yiwei-Test <ee68i5@yeah.net>
2021-01-26 16:27:36 +0800

    test

--- qipai-all/guajiLogin/src/main/java/com/sy/sanguo/common/util/SyPropertiesEncryptFactoryBean.java
+++ qipai-all/guajiLogin/src/main/java/com/sy/sanguo/common/util/SyPropertiesEncryptFactoryBean.java
@@ -1,14 +1,11 @@
 package com.sy.sanguo.common.util;
 
 import com.sy.mainland.util.PropertiesEncryptFactoryBean;
-import org.apache.log4j.Logger;
 
 public class SyPropertiesEncryptFactoryBean extends PropertiesEncryptFactoryBean {
-    private static final Logger logger = Logger.getLogger(SyPropertiesEncryptFactoryBean.class);
     @Override
     public String decrypt(String str) {
         try {
-            logger.info("jdbc|password|2|"+new net.sy599.common.security.SecuritConstantImpl().decrypt(str));
             return new net.sy599.common.security.SecuritConstantImpl().decrypt(str);
         }catch (Exception e){
             return str;
